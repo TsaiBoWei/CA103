47be170e5e69a7f961f3b5730fe3496248f7b90b
Brian Tsai <eliot9010@gmail.com>
2018-10-06 21:45:21 +0800

    Update CA103G1/src/com/plan/controller/PlanAndroidServlet.java

--- CA103G1/src/com/plan/controller/PlanAndroidServlet.java
+++ CA103G1/src/com/plan/controller/PlanAndroidServlet.java
@@ -2,6 +2,7 @@ package com.plan.controller;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.sql.Timestamp;
@@ -12,7 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.eve.model.EventVO;
+import com.eve.model.*;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.plan.model.*;
@@ -20,6 +21,8 @@ import com.plan.model.*;
 @WebServlet("/PlanAndroidServlet")
 public class PlanAndroidServlet extends HttpServlet {
 	private String TAG = "PlanAndroidServlet";
+	private final static String CONTENT_TYPE = "text/html; charset=UTF-8";
+
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
@@ -36,48 +39,36 @@ public class PlanAndroidServlet extends HttpServlet {
 		StringBuilder jsonIn = new StringBuilder();
 		String line = null;
 		
+		while ( (line = br.readLine()) != null)
+			jsonIn.append(line);
+		
+		System.out.println("input: " + jsonIn);
+		
 		List<PlanVO> planList = new ArrayList<PlanVO>();
 		List<EventVO> eventList = new ArrayList<EventVO>();
 		
+		String action = req.getParameter("action");
 		String mem_id = req.getParameter("mem_id");
 		
-		PlanDAO plandao = new PlanDAO();
-		planList = plandao.getPlansByMem(mem_id);
-		
-		
-		super.doPost(req, res);
-	}
-	
-	public class PlanAndroidVO {
-		String plan_id;
-		Timestamp plan_start_date;
-		Timestamp plan_end_date;
-		
-		public PlanAndroidVO( String plan_id, Timestamp plan_start, Timestamp plan_end ) {
-			this.plan_id = plan_id;
-			this.plan_start_date = plan_start_date;
-			this.plan_end_date = plan_end_date;
-		}
-	}
-	
-	public class EventAndroidVO {
-		String eve_id;
-		String eve_title;
-		String eve_content;
-		Timestamp eve_start_date;
-		Timestamp eve_end_date;
-		
-		
-		public EventAndroidVO( String eve_id, String eve_title, String event_content,
-				Timestamp eve_start_date, Timestamp eve_end_date) {
-			this.eve_id = eve_id;
-			this.eve_title = eve_title;
-			this.eve_content = event_content;
-			this.eve_start_date = eve_start_date;
-			this.eve_end_date = eve_end_date;
+		if ( "get_plan_by_mem_id".equals(action) ) {
+			PlanDAO plandao = new PlanDAO();
+			planList = plandao.getPlansByMem(mem_id);
+			writeText(res, gson.toJson(planList));
 		}
 		
-	}	
-	
+		if ("get_eve_by_mem_id".equals(action)) {
+			EveService eveserv = new EveService();
+			eventList = eveserv.getEvesByMem(mem_id);
+			writeText(res, gson.toJson(eventList));
+		}
+	}
+		
+	private void writeText(HttpServletResponse res, String outText) throws IOException {
+		res.setContentType(CONTENT_TYPE);
+		PrintWriter out = res.getWriter();
+		out.print(outText);
+		out.close();
+		System.out.println("outText: " + outText);
+	}
 
 }
