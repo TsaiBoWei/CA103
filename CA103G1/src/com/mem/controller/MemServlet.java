765e611f30316861d271128d75e2bd325fb87928
LoShengHung <lshdurant@gmail.com>
2018-10-17 13:35:27 +0800

    verified email update

--- CA103G1/src/com/mem/controller/MemServlet.java
+++ CA103G1/src/com/mem/controller/MemServlet.java
@@ -197,8 +197,8 @@ public class MemServlet extends HttpServlet {
 				verifyEMail.jedisVeriCode(regaccount, verifyCode);
 				System.out.println("Auth code is: " + verifyCode);
 				String subject = "[Work It Out] 會員認證信";
-				String veriURL = "&quot;"+req.getRequestURL()+"?action=verifyCode&verifyCode="+verifyCode+"&quot;";
-				String messageText = "Hello! " + regName + "<a href="+veriURL+">請輸入此驗證碼:"+"</a>" + verifyCode + "\n";
+				String veriURL = req.getRequestURL()+"?action=verifyCode&verifyCode="+verifyCode;
+				String messageText = "Hello! " + regName + ", 請輸入此驗證碼:"+veriURL + "\n";
 				verifyEMail.sendMail(regaccount, subject, messageText);
 				System.out.println("email has been sent");
 
